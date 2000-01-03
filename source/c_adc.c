651395582a0fb902a0190ef82cd87f74e454f278
Justin Cooper <jwcooper@gmail.com>
2000-01-03 02:00:52 +0000

    initial adc commit

---
+++ source/c_adc.c
@@ -0,0 +1,161 @@
+/*
+Copyright (c) 2013 Adafruit
+Author: Justin Cooper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "c_adc.h"
+
+char ctrl_dir[30];
+char adc_prefix_dir[30];
+char ocp_dir[22];
+int adc_initialized = 0;
+
+
+int load_device_tree(const char *name);
+
+int build_path(const char *partial_path, const char *prefix, char *full_path, size_t full_path_len)
+{
+    DIR *dp;
+    struct dirent *ep;
+
+    dp = opendir (partial_path);
+    if (dp != NULL) {
+        while ((ep = readdir (dp))) {
+            if (strstr(ep->d_name, prefix)) {
+                snprintf(full_path, full_path_len, "%s/%s", partial_path, ep->d_name);
+                (void) closedir (dp);
+                return 1;
+            }
+        }
+        (void) closedir (dp);
+    } else {
+        return 0;
+    }
+
+    return 0;
+}
+
+int initialize_adc(void)
+{
+    if (!adc_initialized && load_device_tree("cape-bone-iio")) {
+        build_path("/sys/devices", "ocp", ocp_dir, sizeof(ocp_dir));
+        build_path(ocp_dir, "helper", adc_prefix_dir, sizeof(adc_prefix_dir));
+        snprintf(adc_prefix_dir, sizeof(adc_prefix_dir), "%s/AIN", adc_prefix_dir);
+        adc_initialized = 1;
+        return 1;
+    }
+
+    return 0;   
+}
+
+int load_device_tree(const char *name)
+{
+    FILE *file = NULL;
+    char slots[40];
+    char line[256];
+
+    build_path("/sys/devices", "bone_capemgr", ctrl_dir, sizeof(ctrl_dir));
+    snprintf(slots, sizeof(slots), "%s/slots", ctrl_dir);
+
+    file = fopen(slots, "r+");
+    if (!file) {
+        return -1;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        //the device is already loaded, return 1
+        if (strstr(line, name)) {
+            fclose(file);
+            return 1;
+        }
+    }
+
+    //if the device isn't already loaded, load it, and return
+    fprintf(file, name);
+    fclose(file);
+
+    return 1;
+}
+
+int unload_device_tree(const char *name)
+{
+    FILE *file = NULL;
+    char slots[40];
+    char line[256];
+    char *slot_line;
+
+    snprintf(slots, sizeof(slots), "%s/slots", ctrl_dir);
+
+    file = fopen(slots, "r+");
+    if (!file) {
+        return -1;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        //the device is loaded, let's unload it
+        if (strstr(line, name)) {
+            slot_line = strtok(line, ":");
+            //remove leading spaces
+            while(*slot_line == ' ')
+                slot_line++;
+
+            fprintf(file, "-%s", slot_line);
+            fclose(file);
+            return 1;
+        }
+    }
+
+    //not loaded, close file
+    fclose(file);
+
+    return 1;
+}
+
+int read(unsigned int *ain)
+{
+    FILE * fh;
+    char ain_path[40];
+    int value = 0;
+    snprintf(ain_path, sizeof(ain_path), "%s/%d", adc_prefix_dir, ain);
+
+    fh = fopen(ain_path, "r");
+
+    fscanf(fh, "%d", &value);
+ 
+    return value;    
+}
+
+int adc_setup()
+{
+    return initialize_adc();
+}
+
+void adc_cleanup(void)
+{
+    unload_device_tree("cape-bone-iio");
+}
---
+++ source/py_adc.c
@@ -0,0 +1,121 @@
+/*
+Copyright (c) 2013 Adafruit
+Author: Justin Cooper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "Python.h"
+#include "constants.h"
+#include "common.h"
+#include "c_adc.h"
+
+static int init_module(void)
+{
+    int i;
+
+    module_setup = 0;
+
+    return 0;
+}
+
+// python function cleanup()
+static PyObject *py_cleanup(PyObject *self, PyObject *args)
+{
+    // unexport the ADC
+    adc_cleanup();
+
+    Py_RETURN_NONE;
+}
+
+// python function setup()
+static PyObject *py_setup_adc(PyObject *self, PyObject *args)
+{
+    adc_setup();
+
+    Py_RETURN_NONE;
+}
+
+// python function read(channel)
+static PyObject *py_read(PyObject *self, PyObject *args)
+{
+    unsigned int ain;
+    char *channel;
+    PyObject *py_value;
+
+    if (!PyArg_ParseTuple(args, "s", &channel))
+        return NULL;
+
+    if (!get_adc_ain(channel, ain)) {
+        PyErr_SetString(PyExc_ValueError, "Invalid AIN key or name.");
+        return NULL;    
+    }
+
+    read(ain);
+
+    py_value = Py_BuildValue("i", value);
+
+    return py_value;
+}
+
+static const char moduledocstring[] = "ADC functionality of a BeagleBone using Python";
+
+PyMethodDef adc_methods[] = {
+    {"setup", py_setup_adc, METH_VARARGS, "Set up and start the ADC channel."},
+    {"read", (PyCFunction)py_read, METH_VARARGS, "Read the analog value for the channel" },
+    {"cleanup", py_cleanup, METH_VARARGS, "Clean up ADC."},
+    //{"setwarnings", py_setwarnings, METH_VARARGS, "Enable or disable warning messages"},
+    {NULL, NULL, 0, NULL}
+};
+
+#if PY_MAJOR_VERSION > 2
+static struct PyModuleDef bbadcmodule = {
+    PyModuleDef_HEAD_INIT,
+    "ADC",       // name of module
+    moduledocstring,  // module documentation, may be NULL
+    -1,               // size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
+    adc_methods
+};
+#endif
+
+#if PY_MAJOR_VERSION > 2
+PyMODINIT_FUNC PyInit_ADC(void)
+#else
+PyMODINIT_FUNC initADC(void)
+#endif
+{
+    PyObject *module = NULL;
+
+#if PY_MAJOR_VERSION > 2
+    if ((module = PyModule_Create(&bbadcmodule)) == NULL)
+       return NULL;
+#else
+    if ((module = Py_InitModule3("ADC", adc_methods, moduledocstring)) == NULL)
+       return;
+#endif
+
+   define_constants(module);
+
+
+#if PY_MAJOR_VERSION > 2
+    return module;
+#else
+    return;
+#endif
+}
