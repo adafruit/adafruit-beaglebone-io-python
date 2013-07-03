6251672fed789b5f2c14b9482a9f30121f17915a
Justin Cooper <jwcooper@gmail.com>
2013-07-03 01:21:47 +0000

    fix firmware copying/removing

--- overlays/builder.py
+++ overlays/builder.py
@@ -1,13 +1,18 @@
 from subprocess import call
 import os
+import shutil
+import glob
 
 def compile():
     call(["dtc", "-O", "dtb", "-o", "overlays/ADAFRUIT-SPI0-00-00A0.dtbo", "-b", "o", "-@", "overlays/ADAFRUIT-SPI0-00-00A0.dts"])
     call(["dtc", "-O", "dtb", "-o", "overlays/ADAFRUIT-SPI0-01-00A0.dtbo", "-b", "o", "-@", "overlays/ADAFRUIT-SPI0-01-00A0.dts"])
     call(["dtc", "-O", "dtb", "-o", "overlays/ADAFRUIT-SPI1-00-00A0.dtbo", "-b", "o", "-@", "overlays/ADAFRUIT-SPI1-00-00A0.dts"])
     call(["dtc", "-O", "dtb", "-o", "overlays/ADAFRUIT-SPI1-01-00A0.dtbo", "-b", "o", "-@", "overlays/ADAFRUIT-SPI1-01-00A0.dts"])
+
 def copy():
-    os.rename("overlays/ADAFRUIT-SPI0-00-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI0-00-00A0.dtbo")
-    os.rename("overlays/ADAFRUIT-SPI0-01-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI0-01-00A0.dtbo")
-    os.rename("overlays/ADAFRUIT-SPI1-00-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI1-00-00A0.dtbo")
-    os.rename("overlays/ADAFRUIT-SPI1-01-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI1-01-00A0.dtbo")
+    for fl in glob.glob("/lib/firmware/ADAFRUIT-SPI*-00A0.dtbo"):
+        os.remove(fl)
+    shutil.move("overlays/ADAFRUIT-SPI0-00-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI0-00-00A0.dtbo")
+    shutil.move("overlays/ADAFRUIT-SPI0-01-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI0-01-00A0.dtbo")
+    shutil.move("overlays/ADAFRUIT-SPI1-00-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI1-00-00A0.dtbo")
+    shutil.move("overlays/ADAFRUIT-SPI1-01-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI1-01-00A0.dtbo")
