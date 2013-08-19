7c130826d666f5e40c2c3b57c13aeb5786e9905f
Justin Cooper <jwcooper@gmail.com>
2013-08-19 21:52:41 +0000

    bump for 0.0.17

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ classifiers = ['Development Status :: 3 - Alpha',
                'Topic :: System :: Hardware']
 
 setup(name             = 'Adafruit_BBIO',
-      version          = '0.0.16',
+      version          = '0.0.17',
       author           = 'Justin Cooper',
       author_email     = 'justin@adafruit.com',
       description      = 'A module to control BeagleBone IO channels',
