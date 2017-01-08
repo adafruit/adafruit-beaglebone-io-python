d6136bf639ca2020d345efb3a9e32b84fef9d8ba
Drew Fustini <drew@pdp7.com>
2017-01-08 10:01:01 +0000

    refactor methods to test all LEDs

--- test/test_led.py
+++ test/test_led.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import errno
 
 import Adafruit_BBIO.GPIO as GPIO
 
@@ -7,16 +8,43 @@ def teardown_module(module):
     GPIO.cleanup()
 
 class TestLED:
-    def test_brightness_high(self):
-        GPIO.setup("USR0", GPIO.OUT)
-        GPIO.output("USR0", GPIO.HIGH)
-        value = open('/sys/class/leds/beaglebone:green:usr0/brightness').read()
-        assert int(value) > 0
+    def set_brightness(self, state, led, name):
+        GPIO.setup(led, GPIO.OUT)
+        GPIO.output(led, state)
+
+        path = "/sys/class/leds/beaglebone:green:{0}/brightness".format(led.lower())
+        value = self.read_led_file(path)
+
+        if value < 0:
+            path = "/sys/class/leds/beaglebone:green:{0}/brightness".format(name)
+            value = self.read_led_file(path)
+
+        if state == 1:
+            assert int(value) > 0
+        else:
+            assert int(value) == 0
         GPIO.cleanup()
 
+    def read_led_file(self, path):
+        try:
+            return open(path).read()
+        except IOError, e:
+            if e.errno == errno.ENOENT:
+                return -1
+
+    def set_all_leds(self, state):
+        test_leds = { "USR0": "heartbeat", \
+                      "USR1": "mmc0", \
+                      "USR2": "cpu0", \
+                      "USR3": "mmc1" }
+        for led, name in test_leds.iteritems():
+            self.set_brightness(state, led, name)
+            GPIO.cleanup()
+   
+    def test_brightness_high(self):
+        self.set_all_leds(GPIO.HIGH)
+
     def test_brightness_low(self):
-        GPIO.setup("USR0", GPIO.OUT)
-        GPIO.output("USR0", GPIO.LOW)
-        value = open('/sys/class/leds/beaglebone:green:usr0/brightness').read()
-        assert int(value) == 0
-        GPIO.cleanup()
+        self.set_all_leds(GPIO.LOW)
+
+
