d727792e286c03dfbfb4f34613401247095485f8
Drew Fustini <drew@pdp7.com>
2017-01-10 10:55:12 +0000

    Silence gcc warning -Wsign-compare
    TODO: fix source the actual cause of the warning:
    source/common.c:344:14: error: comparison between signed and unsigned integer expressions [-Werror=sign-compare]

--- source/common.c
+++ source/common.c
@@ -337,6 +337,8 @@ BBIO_err get_pwm_key(const char *input, char *key)
     return BBIO_INVARG;
 }
 
+// TODO: fix warning
+// source/common.c:344:14: error: comparison between signed and unsigned integer expressions [-Werror=sign-compare]
 BBIO_err get_adc_ain(const char *key, unsigned int *ain)
 {
     *ain = lookup_ain_by_key(key);
