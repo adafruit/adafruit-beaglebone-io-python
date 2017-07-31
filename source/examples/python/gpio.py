87ef745193100ce8b2c2f1d8a83cee49fc413375
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-07-31 15:24:12 -0400

    Added port setup

--- source/examples/python/gpio.py
+++ source/examples/python/gpio.py
@@ -4,6 +4,8 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 LEDs = ["GP0_0", "GP0_1", "GP0_2", "GP0_3", "GP1_0", "GP1_1"]
+for LED in LEDs:
+    GPIO.setup(LED, GPIO.OUT)
 
 while True:
     for LED in LEDs:
