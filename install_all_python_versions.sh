bf97de641a7c73ce4be7af55a3ef98eb245e8757
Drew Fustini <drew@pdp7.com>
2018-08-31 09:22:12 +0000

    update install and test shell scripts
    Signed-off-by: Drew Fustini <drew@pdp7.com>

--- install_all_python_versions.sh
+++ install_all_python_versions.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # useful for testing changes against all versions of python
 make clean
-echo "Install Python 2.7"
-python2.7 ./setup.py install
-echo "Install Python 3.5"
-python3.5 ./setup.py install
-echo "Install Python 3.6"
-python3.6 ./setup.py install
+echo "Install Python 2"
+python2 ./setup.py install
+echo "Install Python 3"
+python3 ./setup.py install
