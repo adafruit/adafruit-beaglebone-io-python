61c29a1ecd6206e2657793a1bed048ea6a95b01d
Daniel Nguyen <44187178+zer0cod3r@users.noreply.github.com>
2020-03-07 14:19:39 -0800

    Update common.c

--- source/common.c
+++ source/common.c
@@ -283,7 +283,7 @@ uart_t uart_table[] = {
   { "PB-UART0", "/dev/ttyO0", "ADAFRUIT-UART0", "P1_30", "P1_32"},
   { "PB-UART1", "/dev/ttyO1", "ADAFRUIT-UART1", "P2_11", "P2_09"},
   { "PB-UART2", "/dev/ttyO2", "ADAFRUIT-UART2", "P1_08", "P1_10"},
-    "PB-UART4", "/dev/ttyO4", "ADAFRUIT-UART4", "P2_05", "P2_07"},
+  { "PB-UART4", "/dev/ttyO4", "ADAFRUIT-UART4", "P2_05", "P2_07"},
   { NULL, NULL, 0, 0, 0 }
 };
 
