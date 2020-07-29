1b2eb9d19dbf3f23aa33d225213c9c606461613a
Drew Fustini <drew@pdp7.com>
2020-07-29 15:34:41 -0500

    Update version in setup.py to v1.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ extension_args = {
 }
 
 setup(name             = 'Adafruit_BBIO',
-      version          = '1.1.1',
+      version          = '1.2.0',
       author           = 'Justin Cooper',
       author_email     = 'justin@adafruit.com',
       description      = 'A module to control BeagleBone IO channels',
